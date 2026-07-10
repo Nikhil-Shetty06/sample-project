400591cf1224e030183a1181382ac3eff41d9162
Nikhil <nikhilshetty006@gmail.com>
2026-07-10 15:16:11 +0530

    from dev2

---
+++ f3.py
@@ -0,0 +1,9 @@
+# Variables and user input
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+# Conditional statement
+if age >= 18:
+    print(f"Hello {name}, you are an adult.")
+else:
+    print(f"Hello {name}, you are a minor.")
